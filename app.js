66c1590dfe44a4beb74c90dd3546a622f7da09a0
Ckbhatia <chetansain86@gmail.com>
2019-11-30 10:17:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const logger = require("morgan");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+if (process.env.NODE_ENV === "development") {
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config");
+  const compiler = webpack(webpackConfig);
+
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    })
+  );
+
+  app.use(require("webpack-hot-middleware")(compiler));
+}
+
+mongoose.connect(
+  "mongodb://localhost/form",
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected to DB");
+    }
+  }
+);
+
+mongoose.set("useCreateIndex", true);
+
+// catch 404 and forward to error handler
+app.use(function(req, res) {
+  res.status(404).json({ message: "Page Not Found" });
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.json({ err });
+});
+
+module.exports = app;
