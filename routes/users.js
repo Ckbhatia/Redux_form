622ab16b54a90720e7a3ffe70193bbafcdf83342
Ckbhatia <chetansain86@gmail.com>
2019-11-30 11:47:48 +0530

    feat: add put route

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.post("/register", async (req, res) => {
     const user = await User.create(req.body);
     res.json({ user, status: "success" });
   } catch (error) {
-    res.json({ message: "There's an error", status: "failed" });
+    res.status(400).json({ message: "There's an error", status: "failed" });
   }
 });
 
@@ -19,7 +19,22 @@ router.get("/", async (req, res) => {
     if (!users) res.json({ message: "No users found", status: "failed" });
     res.json({ users, status: "failed" });
   } catch (error) {
-    res.json({ message: "There's an error", status: "failed" });
+    res.status(400).json({ message: "There's an error", status: "failed" });
+  }
+});
+
+// Put
+router.put("/:username", async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOneAndUpdate({ name: username }, req.body, {
+      new: true
+    });
+    console.log(user, "user");
+    if (!user) res.json({ message: "No user found", status: "failed" });
+    res.json({ user, status: "failed" });
+  } catch (error) {
+    res.status(400).json({ message: "There's an error", status: "failed" });
   }
 });
 
