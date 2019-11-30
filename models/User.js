2c62199f6373a8e00e9bcbec93b67a92d7dc37ac
Ckbhatia <chetansain86@gmail.com>
2019-11-30 11:21:32 +0530

    feat: add user router with post route

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
