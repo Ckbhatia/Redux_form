8a4963e4d1dfd8a127e399379f7314ae78ed6684
Ckbhatia <chetansain86@gmail.com>
2019-12-01 21:48:25 +0530

    feat: add getUsers action creator with todo

---
+++ client/actions/actions.js
@@ -0,0 +1,14 @@
+import {
+  FETCHING_STARTED,
+  SET_ERROR,
+  GET_USERS,
+  REGISTER_USER,
+  DELETE_USER,
+  UPDATE_USER
+} from "./types";
+
+export const getUsers = () => {
+  /* TODO: Add fetch users funtionality
+   * Dispatch the action whenever fetch get done etc.
+   */
+};
