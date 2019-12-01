6b262bd77f0aed82f0a262294aaffeda61dc42d2
Ckbhatia <chetansain86@gmail.com>
2019-12-01 21:40:11 +0530

    feat: add action types

---
+++ client/actions/types.js
@@ -0,0 +1,6 @@
+export const REGISTER_USER = "REGISTER";
+export const UPDATE_USER = "UPDATE";
+export const DELETE_USER = "DELETE";
+export const GET_USERS = "GETLIST";
+export const FETCHING_STARTED = "FETCHING_STARTED";
+export const SET_ERROR = "SET_ERROR";
