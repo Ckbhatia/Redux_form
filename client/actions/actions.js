455d4683b54f5b46eee1225c0d344855ddbed64d
Ckbhatia <chetansain86@gmail.com>
2019-12-02 10:43:02 +0530

    feat: add register user action creator

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -2,6 +2,7 @@ import {
   FETCHING_STARTED,
   SET_ERROR,
   GET_USERS,
+  GET_USER,
   REGISTER_USER,
   DELETE_USER,
   UPDATE_USER
@@ -14,6 +15,22 @@ export const getUsers = () => {
       dispatch({ type: FETCHING_STARTED });
       const res = await axios.get("http://localhost:3000/api/v1/users");
       dispatch({ type: GET_USERS, value: res.data.users });
+    } catch (err) {
+      dispatch({ type: SET_ERROR });
+      console.error(err);
+    }
+  };
+};
+
+export const registerUser = value => {
+  return async dispatch => {
+    try {
+      dispatch({ type: FETCHING_STARTED });
+      const res = await axios.post(
+        "http://localhost:3000/api/v1/users/register",
+        value
+      );
+      dispatch({ type: REGISTER_USER, value: res.data.user });
     } catch (err) {
       dispatch({ type: SET_ERROR });
       console.error(err);
