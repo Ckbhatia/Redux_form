00a056d53659022ff96085c62dc2eeb5b7852a0a
Ckbhatia <chetansain86@gmail.com>
2019-12-02 10:50:05 +0530

    feat: add users register route

--- client/App.js
+++ client/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import Home from "./components/Home";
+import RegisterForm from "./components/RegisterForm";
 
 function App(props) {
   return (
     <Switch>
       {/* NOTE: The first route is temporary solution, check it */}
       <Route exact path="/" render={() => props.history.push("/users")} />
-      <Route path="/users" component={Home} />
+      <Route exact path="/users" component={Home} />
+      <Route exact path="/users/register" component={RegisterForm} />
     </Switch>
   );
 }
