cfcefe09bf5b03f5d6732863889558e805d95b9a
Ckbhatia <chetansain86@gmail.com>
2019-12-02 10:23:12 +0530

    feat: add new variable GET_USER

--- client/actions/types.js
+++ client/actions/types.js
@@ -2,5 +2,6 @@ export const REGISTER_USER = "REGISTER";
 export const UPDATE_USER = "UPDATE";
 export const DELETE_USER = "DELETE";
 export const GET_USERS = "GETLIST";
+export const GET_USER = "GETUSER";
 export const FETCHING_STARTED = "FETCHING_STARTED";
 export const SET_ERROR = "SET_ERROR";
