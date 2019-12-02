0a623e8ade4ca2f1414ef036d783dd4eca0794a6
Ckbhatia <chetansain86@gmail.com>
2019-12-02 16:20:17 +0530

    feat: add list items and delete user functionality

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getUsers } from "../actions/actions";
+import { getUsers, deleteUser } from "../actions/actions";
 import Loader from "./Loader";
 
 class Home extends Component {
@@ -9,6 +9,12 @@ class Home extends Component {
     this.props.getUsers();
   }
 
+  handleClick = e => {
+    if (e.target.className === "delete-btn") {
+      this.props.deleteUser(e.target.dataset.key);
+    }
+  };
+
   render() {
     return (
       <div className="main-container">
@@ -16,12 +22,18 @@ class Home extends Component {
           <Loader />
         ) : (
           <div className="users-list-container">
-            <ul className="list-container">
+            <ul onClick={this.handleClick} className="list-container">
               {this.props.users.length > 0 &&
                 this.props.users.map(user => {
                   return (
                     <li key={user._id} className="user-list-item">
-                      {user.name}
+                      <div className="item-container">
+                        <span className="user-name">{user.name}</span>
+                        <span className="user-email">{user.email}</span>
+                        <button className="delete-btn" data-key={user.name}>
+                          delete
+                        </button>
+                      </div>
                     </li>
                   );
                 })}
@@ -42,4 +54,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { getUsers })(Home);
+export default connect(mapStateToProps, { getUsers, deleteUser })(Home);
