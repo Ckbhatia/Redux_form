ef3e37fc21e8b1286fa8492516bf6ad0d29d3a6a
Ckbhatia <chetansain86@gmail.com>
2019-12-02 16:20:39 +0530

    feat: add delete user action creator

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -52,3 +52,18 @@ export const getUser = username => {
     }
   };
 };
+
+export const deleteUser = username => {
+  return async dispatch => {
+    try {
+      dispatch({ type: FETCHING_STARTED });
+      const res = await axios.delete(
+        `http://localhost:3000/api/v1/users/${username}`
+      );
+      dispatch({ type: DELETE_USER, value: username });
+    } catch (err) {
+      dispatch({ type: SET_ERROR });
+      console.error(err);
+    }
+  };
+};
