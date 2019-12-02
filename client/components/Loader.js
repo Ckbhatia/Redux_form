eb782a817e7757fc9a1eb8360d1f7ccb64aaa4c9
Ckbhatia <chetansain86@gmail.com>
2019-12-02 16:19:30 +0530

    feat: add a loader-container

--- client/components/Loader.js
+++ client/components/Loader.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Loader() {
-  return <div className="lds-hourglass"></div>;
+  return (
+    <div className="loader-container">
+      <div className="lds-hourglass"></div>
+    </div>
+  );
 }
